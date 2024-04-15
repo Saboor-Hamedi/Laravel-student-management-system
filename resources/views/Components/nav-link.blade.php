c5551c0aafd102729ce3995ff246d9cd711fdd78
Saboor-Hamedi <janhamedi49@gmail.com>
2024-04-15 13:34:23 +0700

    first commit

---
+++ resources/views/Components/nav-link.blade.php
@@ -0,0 +1,21 @@
+@props(['active' => false, 'href' => '#'])
+
+
+
+{{-- <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}
+ rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
+    {{ $slot }}
+</a> --}}
+
+{{-- check if href is active make it inactive, if not active make it active--}}
+@if ($active)
+    <button class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">
+        {{ $slot }}
+    </button>
+@else
+    <a href="{{ $href }}"
+       class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+        {{ $slot }}
+    </a>
+@endif
---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <x-slot:heading>
+    About
+  </x-slot:heading>
+  <h1>I am about</h1>
+</x-layout>
