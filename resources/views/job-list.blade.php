a7564ffbab0d221916131fa71526da70ae890b61
Saboor-Hamedi <janhamedi49@gmail.com>
2024-04-19 19:44:56 +0700

    fetch data from Jobs model, hard coded

--- resources/views/job-list.blade.php
+++ resources/views/job-list.blade.php
@@ -2,7 +2,5 @@
     <x-slot:heading>
         Jobs
     </x-slot:heading>
-
         <li>{{ $job['title'] }}: Paid in a year {{ $job['salary'] }}</li>
-
     </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Jobs;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
@@ -10,49 +12,9 @@ Route::get('/about', function () {
     return view('/about');
 });
 Route::get('/jobs', function () {
-    return view('/jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Web Developer',
-                'salary' => 10000
-            ],
-            [
-                'id' => 2,
-                'title' => 'Web Designer',
-                'salary' => 5000
-            ],
-            [
-                'id' => 3,
-                'title' => 'Graphic Designer',
-                'salary' => 7000
-            ]
-        ]
-    ]);
+    return view('/jobs', ['jobs' => Jobs::all()]);
 });
 Route::get('/job-list/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Web Developer',
-            'salary' => 10000
-        ],
-        [
-            'id' => 2,
-            'title' => 'Web Designer',
-            'salary' => 5000
-        ],
-        [
-            'id' => 3,
-            'title' => 'Graphic Designer',
-            'salary' => 7000
-        ]
-    ];
-    $job =  Arr::first($jobs, fn ($job) => $job['id'] == $id);
-    // foreach ($jobs as $job) {
-    //     if ($job['id'] == $id) {
-    //         return view('job-list', ['job' => $job]);
-    //     }
-    // }
+    $job = Jobs::find($id);
     return view('job-list', ['job' => $job]);
 });
